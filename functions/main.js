5597c84ba262ef91d675349360942e0a7e9d4cef
Alexey Nikolaevskiy <anikolaevski@gmail.com>
2019-04-03 01:07:52 +0300

    Lesson#3 - functions

--- functions/main.js
+++ functions/main.js
@@ -30,3 +30,47 @@ function showSolutionsMessage(a,b,c) {
 showSolutionsMessage(1,2,3);
 showSolutionsMessage(2,4,2);
 showSolutionsMessage(7,20,-3);
+
+//Task#2
+function getPersonData( secretData ) {
+  let dName = function ( n ) {
+    return (n == 0)?'Родриго':'Эмильо'
+    }
+  return { firstName: dName(secretData.aaa), lastName: dName(secretData.bbb) };
+}
+console.log(getPersonData({ aaa:0, bbb:0 }));
+console.log(getPersonData({ aaa:0, bbb:1 }));
+console.log(getPersonData({ aaa:1, bbb:0 }));
+console.log(getPersonData({ aaa:1, bbb:1 }));
+
+//Task#3
+function getAverageScore( data ) {
+  let result = new Object();
+  let avgVal = function ( dat ) {
+    let mySum = 0;
+    for (let i=0; i<dat.length; i++) {
+      mySum += dat[i];
+    }
+    return (dat.length>0)?mySum / dat.length:0;
+   };
+  let resultArray = [];
+  for (let prop in data ) {
+    result[prop] = avgVal(data[prop]);
+    resultArray.unshift(avgVal(data[prop]));
+   }
+   result.average = avgVal(resultArray);
+  return result;
+}
+console.log(getAverageScore(
+{
+  algebra:[2,4,5,2,3,4],
+  geometry:[2,4,5],
+  russian:[3,3,4,5],
+  physics:[5,5],
+  music:[2,2,6],
+  english:[4,4,3],
+  poetry:[5,3,4],
+  chemistry:[2],
+  french:[4,4]
+}
+));
