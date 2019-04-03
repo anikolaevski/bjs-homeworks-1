688ed8362c3f6bd01027269c6554a0e5721199a5
Alexey Nikolaevskiy <anikolaevski@gmail.com>
2019-04-03 19:46:08 +0300

    Lesson#4, wrappers-for-primitives

--- wrappers-for-primitives/task.js
+++ wrappers-for-primitives/task.js
@@ -1,8 +1,9 @@
+"use strict";
 function calculateMortgage() {
     let percent = window.percent.value;
     let contribution = window.contribution.value;
     let amount = window.amount.value;
-    let date = window.date.value;
+    let date = new Date(window.date.value); //Did not work without Date() creation
 
     let result = calculateTotalMortgage(percent, contribution, amount, date);
     let span = window.mortageResult;
@@ -10,9 +11,20 @@ function calculateMortgage() {
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
-
     // код для задачи №1 писать здесь
-    //return totalAmount;
+    let md = function (d1, d2) {
+      let months;
+      months = (d2.getFullYear() - d1.getFullYear()) * 12;
+      months -= d1.getMonth();
+      months += d2.getMonth();
+      return months <= 0 ? 0 : months;
+    }
+    let P = percent / 1200;
+    let D = new Date();
+    let n = md( D, date );
+    let monthAmount = (amount - contribution) * (P + P / ((Math.pow((1 + P), n)) - 1 ));
+    let totalAmount = monthAmount * n;
+    return totalAmount;
 }
 
 function sayHello() {
@@ -23,6 +35,6 @@ function sayHello() {
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    //return greeting;
+    let result = 'Здравствуйте! Меня зовут '+((name)?name:'Аноним');
+    return result;
 }
