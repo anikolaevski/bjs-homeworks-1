617e3f5912e70041638a2d315ddf5ac5bee58269
anikolaevski <anikolaevski@gmail.com>
2019-04-25 23:30:33 +0300

    corrected

--- async/main.js
+++ async/main.js
@@ -1,38 +1,24 @@
-//Task#1
-function setAlarm(time, callback) {
-  const curTime = new Date();
-  const curYear = curTime.getFullYear();
-  const curMonth = curTime.getMonth();
-  const curDay = Math.floor(curTime.getDate());
-  const paramTime = time.split(':');
-  const paramHour = parseInt(paramTime[0],10);
-  const paramMin = parseInt(paramTime[1],10);
-  const calcDate = new Date(curYear, curMonth, curDay, paramHour, paramMin,0,0);
-  const diff = Math.abs(calcDate - curTime);
-  const f = function() {
-    const args = Array.from(arguments);
-    const result = callback.apply(null, args);
-    return result;
-  }
-//  console.log(diff);
-  if(diff < 5000) {
-    return f();
-  }
-}
-//тест
-const workCheck = '10:00';
-const StartWorking = () => alert('Хорош околачивать груши! Работать!'); 
-const checkTime = () => setAlarm(workCheck, StartWorking);
+function setAlarm (time, message, callback) {
+    const ct = get_hhmm_time();
+    if(ct == time) {
+      callback(message);
+    }
+ }
 
-console.log(checkTime);
+ function get_hhmm_time() {
+  const now = new Date();
+  let now_hours = now.getHours()+'';
+  let now_mins = now.getMinutes()+'';
+  while (now_hours.length < 2) { now_hours  = "0" + now_hours;}
+  while (now_mins.length < 2) { now_mins  = "0" + now_mins;}
+ return now_hours+':'+now_mins;
+}
+ 
+//тест
+
+setAlarm('23:29', 'Пора сдавать работу!', (message) => {alert(message)});
 
 function setDailyRhythm(wakeUpTime, bedTime) {
-  const wakeUp = () => alert('Просыпайся!');
-  const go2bed = () => alert('Спокойной ночи!');
-  const checkTime01 = () => setAlarm(wakeUpTime, wakeUp);
-  const checkTime02 = () => setAlarm(bedTime, go2bed);
-  setInterval(checkTime01, 5000); // Каждые 5 сек
-  setInterval(checkTime02, 5000);
+  setInterval(setAlarm(wakeUpTime, 'Пора просыпаться!', (message) => {alert(message)}), 60000); 
+  setInterval(setAlarm(bedTime, 'Пора в кровать!', (message) => {alert(message)}), 60000);
 }
-
-setDailyRhythm('07:00', '23:00');
