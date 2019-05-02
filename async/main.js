110844fdf6ec04b730fe09a10755a966ef1fc036
anikolaevski <anikolaevski@gmail.com>
2019-05-02 21:09:29 +0300

    Corrected

--- async/main.js
+++ async/main.js
@@ -1,21 +1,25 @@
 function setAlarm (time, callback) {
-  const ct = get_hhmm_time();
-  if(ct == time) {
-    return callback;
-  }
+    const d = new Date();
+    // return () => callback(time,d);
+    const makeTimeSecs = (alert) => {return alert + ((alert.length == 5)?':00':'')};
+    const get_hhmmss_time = (time) => {
+      const f = (x) => { let y = x + ''; return (y.length < 2)? '0' + y: y };
+      return f(time.getHours())+':'+f(time.getMinutes())+':'+f(time.getSeconds());
+    }
+    // console.log(makeTimeSecs(time), get_hhmmss_time(d));
+    if(makeTimeSecs(time) == get_hhmmss_time(d)) {
+      callback();
+    }
 }
 
-function get_hhmm_time() {
-  const now = new Date();
-  const f = (x) => { let y = x + ''; return (y.length < 2)? '0'+y: y };
-  return f(now.getHours())+':'+f(now.getMinutes());
+function setDailyRhythm(arg1, arg2) {
+  const GoodMorning = () => alert('Вставай, Вася!');
+  const GoodNight = () => alert('Пора ложиться, Вася!');
+  const wakeUp = () => setAlarm(arg1, GoodMorning);
+  const gotoBed = () => setAlarm(arg2, GoodNight);
+  setInterval(wakeUp,1000);
+  setInterval(gotoBed,1000);
 }
 
-function setDailyRhythm(wakeUpTime, bedTime) {
-  const f1 = setAlarm(wakeUpTime, () => alert('Пора просыпаться!'));
-  if(f1) {f1()};
-  const f2 = setAlarm(bedTime, () => alert('Пора в кровать!'));
-  if(f2) {f2()};
-}
-
-setInterval(setDailyRhythm('07:00','23:00'),60000); // 1 раз в минуту
+setDailyRhythm('07:00','23:00');
+// setDailyRhythm('21:07:55','21:08:15');
